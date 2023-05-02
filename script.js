967ef84d6e8445a020966ccb5d1be160e7d29e34
Galex <43304388+GalexDL@users.noreply.github.com>
2023-05-02 14:22:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-// Create a scene
-var scene = new THREE.Scene();
-
-// Create a camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Create a renderer
-var renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("canvas") });
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Load the 3D model
-var loader = new THREE.FBXLoader();
-loader.load('Goodra.fbx', function (object) {
-  scene.add(object);
-});
-
-// Add animation loop
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-animate();
+    function openPopup() {
+      var popup = document.getElementById("popupMenu");
+      if (popup.style.display === "none") {
+        popup.style.display = "block";
+      } else {
+        popup.style.display = "none";
+      }
+    }
