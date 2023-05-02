a514fe7550d0bd2407a9f6243040735167a8960a
Galex <43304388+GalexDL@users.noreply.github.com>
2023-05-02 12:28:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Create a scene
+var scene = new THREE.Scene();
+
+// Create a camera
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Create a renderer
+var renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("canvas") });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Load the 3D model
+var loader = new THREE.FBXLoader();
+loader.load('Goodra.fbx', function (object) {
+  scene.add(object);
+}, undefined, function (error) {
+  console.error(error);
+});
+
+// Add animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
