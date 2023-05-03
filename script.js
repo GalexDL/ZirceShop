4c8040441290cfb1563d65fdde70c115313d1517
Galex <43304388+GalexDL@users.noreply.github.com>
2023-05-03 12:40:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ var mainMenu = document.getElementById("mainMenu");
 menuButton.addEventListener("click", function() {
   mainMenu.classList.toggle("open");
 });
+
+var composer = new THREE.EffectComposer(renderer);
+
+var renderPass = new THREE.RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+var edgeDetectionPass = new THREE.ShaderPass(THREE.EdgeShader2);
+edgeDetectionPass.uniforms.uNormals.value = 1;
+composer.addPass(edgeDetectionPass);
+
+var outlinePass = new THREE.ShaderPass(THREE.OutlineShader);
+outlinePass.uniforms.uEdgeThickness.value = 0.1;
+outlinePass.uniforms.uEdgeColor.value.set(0xff0000);
+outlinePass.renderToScreen = true;
+composer.addPass(outlinePass);
